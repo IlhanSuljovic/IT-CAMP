666ffe0d8dd9d4ed4af50a5496bb3276548beb12
IlhanSuljovic <suljovic.ilhan123@gmail.com>
2023-10-08 16:44:59 +0200

    Javascript

---
+++ nesto.js
@@ -0,0 +1,129 @@
+// console.log(1, 1, 1 + 1, 2 + 2, 3+3, 4 + 4);
+
+///////////////////////////////////////////////////////5.7 a
+
+// let fp = 1, fpp = 1;
+
+// let n = parseInt(prompt("unesite broj elemenata"))
+
+// let rowOutput = '1 1';
+
+// let i = 3
+
+// while(i <= n){
+//     let pom = fp + fpp
+//     rowOutput += `${pom}`
+//     fp = fpp;
+//     fpp = pom;
+//     i++
+// }
+
+// console.log(rowOutput);
+
+
+
+
+
+/////////////////////////////////////////////////5.8 b)
+
+// let fp = 1, fpp = 1;
+
+// let n = parseInt(prompt("unesite broj elemenata"))
+
+// let rowOutput = '1 1';
+
+// let i = 3, suma = 2;
+
+// while(i <= n){
+//     let pom = fp + fpp
+//     rowOutput += `${pom}`
+//     fp = fpp;
+//     fpp = pom;
+//     i++
+// }
+
+// console.log(rowOutput);
+
+// let n = parseInt(prompt("Unesite broj"))
+// let m = parseInt(prompt("Unesite stepen broja"))
+
+// console.log(Math.pow(n, m));
+
+// let i = 0, proizvod = 1;
+
+// while(i<a){
+//     proizvod *= n
+//     i++
+// }
+// console.log(proizvod);
+
+////////////////////5.9
+
+// let suma = 0
+
+// let stop = parseFloat(promt("unesite broj:"))
+
+// while(stop !== 0){
+//     suma+= stop
+    
+// }
+
+
+///////////////////5.15
+
+
+// let n = parseInt(prompt("unesite broj:"))
+
+// let suma = 0;
+
+
+// let pom=n;  
+
+// while(pom >= 1){
+//     suma += pom%10
+//  pom = Math.floor(  pom/=10)
+
+// }
+
+// if(n % suma === 0)
+
+///////////////////////////////////////////////////////////////////////////
+
+
+// let n = parseInt(prompt("Unesite broj:"))
+// let k = 2;for( let k = 2; k <= n; k++){
+//     while( n % k === 0){
+//     n /= k;
+//     console.log(k);
+//     }
+
+// }
+
+
+// for (let n = 1; n <= 20; n++) {
+//     for (let b = 1; b <= 20; b++) {
+      
+//         let c = Math.sqrt(a ** 2 + b ** 2);
+
+
+//         if (Number.isInteger(c)) {
+//             console.log(`Pitagorina trojka: (${a}, ${b}, ${c})`);
+//         }
+//     }
+// }
+
+
+//////////5.21
+
+// for(let a = 1; a <= 20; a++){
+
+// for(let b = 1; b <= 20; b++){
+//     let zbir = a*a + b*b;
+//     let c=1
+//     while(c*c >= zbir)
+//     if(c*c)
+// }
+// }
+
+
+
